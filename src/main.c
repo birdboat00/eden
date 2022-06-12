0610bdedaeed865b13275cf40955b4e837785cc2
birdboat00 <roidl.sebastian@outlook.com>
2022-06-12 16:07:11 +0200

    Added abilitiy to interpret file from filepath - removed --interptestpack command - added ability to load pack file from filename

--- src/main.c
+++ src/main.c
@@ -18,16 +18,22 @@ void print_version() {
 void print_usage() {
   printf("Usage: eden command [options]\n\n"
     "Commands:\n\n"
+    "  <filename.eden>          Interpret file <filename.eden> and exit\n"
     "  version --version  -v    Print version information and exit\n"
     "  help    --help     -h    Print this help message and exit\n\n"
     "Temporary Commands:\n\n"
     "  --dumptestpack           Dumps the integrated test pack to dump.txt\n"
     "  --savetestpack           Exports the integrated test pack to code.eden\n"
-    "  --readtestpack           Reads the code.eden pack file\n"
-    "  --interptestpack         Interpret the integrated test pack\n");
+    "  --readtestpack           Reads the code.eden pack file\n");
 }
 
 int main(u32 argc, str* argv) {
+
+  if (argc <= 1) {
+    print_usage();
+    return 0;
+  }
+
   if (has_arg("version", argc, argv) || has_arg("--version", argc, argv) || has_arg("-v", argc, argv)) {
     print_version();
     return 0;
@@ -37,15 +43,15 @@ int main(u32 argc, str* argv) {
     return 0;
   }
 
-  edn_pack_t test_pack = create_test_pack();
-
   if (has_arg("--dumptestpack", argc, argv)) {
     FILE* file = fopen("dump.txt", "w");
+    edn_pack_t test_pack = create_test_pack();
     edn_dump_pack(isnull(file) ? stdout : file, &test_pack);
     fclose(file);
     return 0;
   } else if (has_arg("--savetestpack", argc, argv)) {
     FILE* outfile = fopen("code.eden", "wb");
+    edn_pack_t test_pack = create_test_pack();
     edn_dump_pack(stdout, &test_pack);
     edn_err_t err = edn_write_pack(outfile, &test_pack);
     if (!edn_err_is_ok(err)) printf("failed to write pack: %i\n", err);
@@ -61,18 +67,38 @@ int main(u32 argc, str* argv) {
     return err.kind;
   }
 
-  edn_vm_t* vm = edn_make_vm(&test_pack, (edn_vm_params_t) { .verbose = 1 });
-  if (isnull(vm)) return kErrMallocFail;
-
-  edn_err_t err = edn_run_vm(vm);
-  if (!edn_err_is_ok(err)) { 
-    printf("VM ERROR - dumping registers...\n");
-    for (usize i = 0; i < arraylen(edn_reg_t, vm->registers); i++) {
-      char buf[256];
-      edn_term_to_str(&(vm->registers[i]), buf, 256);
-      printf("r%i = %s\n", i, buf);
+  if (argc == 2) {
+    FILE* file = fopen(argv[1], "rb");
+    if (isnull(file)) {
+      printf("failed to open file '%s'.\n", argv[1]);
+      return kErrInvalidFile;
     }
+
+    edn_pack_t pack;
+    const edn_err_t err = edn_read_pack(file, &pack);
+    if (!edn_err_is_ok(err)) {
+      printf("failed to read pack file: %s\n", edn_err_to_str(&err));
+      return err.kind;
+    }
+
+    edn_vm_t* vm = edn_make_vm(&pack, (edn_vm_params_t) { .verbose = 1 });
+    if (isnull(vm)) {
+      printf("failed to allocate vm memory.\n");
+      return kErrMallocFail;
+    }
+
+    const edn_err_t runerr = edn_run_vm(vm);
+    if (!edn_err_is_ok(runerr)) {
+      printf("VM ERROR - dumping register...\n");
+      for(usize i = 0; i < arraylen(edn_reg_t, vm->registers); i++) {
+        char buf[256];
+        edn_term_to_str(&(vm->registers[i]), buf, 256);
+        printf("r%i = %s\n", i, buf);
+      }
+    }
+
+    return runerr.kind;
   }
-  
-  return err.kind;
+
+  return 0;
 }
